b03fb6b6bbf27aaf7c98969430820dce91fe6a0b
Alinebarbosaa <alineemille4@gmail.com>
2025-09-17 13:58:07 -0300

    Ex parte 2

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
-app.get('/usuarios', function (request, response) {
+// - Query params => meusite.com/users?nome=aline&age=39 // FILTROS 
+//- Route params => /users/2    BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÍFICO
+// - Request Body => {"name":"Aline", "age":39 } 
 
-    response.send(Aline);
+app.get('/usuarios/:id', function (request, response) {
+    console.log(request)
     
+
+    response.send('Batata com Abacate')
 
 })
 
-app.listen(3000, () => {
-    console.log("Meu servidor ta rodando UHUUUU")
+app.listen(3001, () => {
+    console.log('Meu servidor ta rodando UHUUUU')
 })
