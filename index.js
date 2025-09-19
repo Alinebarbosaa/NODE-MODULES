ea8bad6a30b6b69e1010e2937d18a402a260169b
Alinebarbosaa <alineemille4@gmail.com>
2025-09-19 15:10:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { useSyncExternalStore } = require('react')
 
 const app = express()
+app.use(express.json())
+
 
 // - Query params => meusite.com/users?nome=aline&age=39 // FILTROS 
 //- Route params => /users/2    BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÍFICO
@@ -8,18 +11,45 @@ const app = express()
 
 // VERBOS HTTP
 
-//-GET => Buscar informação no back-end
+//-GET => Buscar informação no back-end / Listar uma informaçao
 //-POST => Criar informação no back-end
 //-PUT / PATCH => Alteração/Atualização informação no back-end
 //-DELETE => Deletar informação no back-end
 
-app.get('/usuarios/:id', function (request, response) {
-    console.log(request)
+//JSON => JAVASCRIPT OBJECT NOTATION
 
+                    
+const user = [
+    {
+    name: 'Aline',
+    age: 39
+ },
+]
 
-    response.send('Batata com Abacate')
+app.get('/usuarios/', function (request, response) {
+    response.json(user)
+})
+
+app.post('/usuarios/id:', function (request, response) {
+    console.log(request.body)
+
+    const newUser = request.body
+
+    user.push(newUser)
+
+    response.status(201).json(newUser)
 
 })
+
+app.delete('/usuarios/id:', function (request, response) {
+    console.log(request.params.id)
+    const id = request.params.id
+
+    user.splice(id, 1)
+
+    response.send("usuario deletado com sucesso!")
+
+})   
 
 app.listen(3001, () => {
     console.log('Meu servidor ta rodando UHUUUU')
